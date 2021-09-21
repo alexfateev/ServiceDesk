793c2e002ae7a1aca72a1e739ed1347565e0649e
Alexandr Fateev <mailboxfav@gmail.com>
2021-09-21 14:42:24 +0500

    fix bugs

--- src/main/java/himmash/local/MainApp.java
+++ src/main/java/himmash/local/MainApp.java
@@ -2,11 +2,14 @@ package himmash.local;
 
 import himmash.local.database.DBHandler;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import static himmash.local.database.Const.*;
@@ -19,11 +22,16 @@ public class MainApp extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "." + "" +
+        Label label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + "." + "" +
                 "\nComputerName: " + SystemInfo.getComputerName() + ", User: " + SystemInfo.getFullUserName());
-        var scene = new Scene(new StackPane(label), 640, 480);
+        ImageView image = new ImageView("file:resources/cat_upsidedown.png");
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.TOP_CENTER);
+        vBox.setSpacing(10);
+        vBox.getChildren().addAll(image,label);
+        var scene = new Scene(new StackPane(vBox), 640, 480);
 
-        stage.getIcons().add(new Image("file:resources/haski.png"));
+        stage.getIcons().add(new Image("file:resources/cat_pirate.png"));
         stage.setTitle(titleApp);
         stage.setScene(scene);
         stage.show();
