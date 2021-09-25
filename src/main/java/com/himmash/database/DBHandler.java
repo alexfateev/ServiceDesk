30c5aeaf1773a27f7242e00bdc1cf88ed511b5e2
Fateev_AV <mailboxfav@gmail.com>
2021-09-25 16:30:08 +0500

    some changes

--- src/main/java/com/himmash/database/DBHandler.java
+++ src/main/java/com/himmash/database/DBHandler.java
@@ -1,5 +1,7 @@
 package com.himmash.database;
 
+import com.himmash.utils.Config;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
---
+++ src/main/java/com/himmash/utils/Config.java
@@ -0,0 +1,35 @@
+package com.himmash.utils;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+
+    private static final String CONFIG_NAME = "config.properties";
+    private static final Properties CONFIG = new Properties();
+
+    public static void initConfig() throws IOException {
+        initConfig(null);
+    }
+
+    public static void initConfig(String name) throws IOException {
+        if (name != null && !name.trim().isEmpty()) {
+            CONFIG.load(new FileReader(name));
+        } else {
+            CONFIG.load(new FileReader(CONFIG_NAME));
+        }
+    }
+
+    public static String getProperty(String propertyName) {
+        return CONFIG.getProperty(propertyName);
+    }
+
+    public static final String dbHost = "192.168.128.60";
+    public static final String dbPort = "3306";
+    public static final String dbName = "db_service_desk_cat_edition";
+    public static final String dbUser = "user_cat";
+    public static final String dbPass = "";
+    public static final String dbParam = "?useUnicode=true&serverTimezone=UTC";
+
+}
