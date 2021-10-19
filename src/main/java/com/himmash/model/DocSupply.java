01c9bc325716e09879f87f455574b1f94260c31c
mailboxfav@gmail.com <Antisith777>
2021-10-19 15:40:58 +0500

    some changes

---
+++ src/main/java/com/himmash/model/DocSupply.java
@@ -0,0 +1,14 @@
+package com.himmash.model;
+
+public class DocSupply {
+    /*
+    Класс описывающий документ поступления
+    Содержит следущие поля
+    -id
+    -Поставщик (vendor)
+    -Дата документа (date)
+    -Наименование (name)
+    -Комментарий (comment)
+    -Сохранен (saved)
+     */
+}
--- src/main/java/com/himmash/model/EquipmentType.java
+++ src/main/java/com/himmash/model/EquipmentType.java
@@ -3,5 +3,8 @@ package com.himmash.model;
 public class EquipmentType {
     /*
     Класс описывающий тип техники {Монитор, системный блок и т.п.}
+    Содержит следующие поля:
+    -id
+    -наименование (name)
      */
 }
