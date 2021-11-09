9d77024b0e7deb9694a03f6231b51562effaf3e4
Lemeshko.P <zloy.palkan@gmail.com>
2021-11-09 10:08:37 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/himmash/model/Division.java
    #	src/main/java/com/himmash/model/Employee.java

--- src/main/java/com/himmash/model/DocSupply.java
+++ src/main/java/com/himmash/model/DocSupply.java
@@ -1,5 +1,12 @@
 package com.himmash.model;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
+
+import java.time.LocalDate;
+
 public class DocSupply {
     /*
     Класс описывающий документ поступления
@@ -11,4 +18,96 @@ public class DocSupply {
     -Комментарий (comment)
     -Сохранен (saved)
      */
+    private StringProperty vendor;
+    private StringProperty name;
+    private StringProperty commet;
+    private LongProperty id;
+    private LongProperty pid;
+    private BooleanProperty saved;
+    private ObjectProperty<LocalDate> date;
+
+    public String getVendor() {
+        return vendor.get();
+    }
+
+    public StringProperty vendorProperty() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor.set(vendor);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getCommet() {
+        return commet.get();
+    }
+
+    public StringProperty commetProperty() {
+        return commet;
+    }
+
+    public void setCommet(String commet) {
+        this.commet.set(commet);
+    }
+
+    public long getId() {
+        return id.get();
+    }
+
+    public LongProperty idProperty() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id.set(id);
+    }
+
+    public long getPid() {
+        return pid.get();
+    }
+
+    public LongProperty pidProperty() {
+        return pid;
+    }
+
+    public void setPid(long pid) {
+        this.pid.set(pid);
+    }
+
+    public boolean isSaved() {
+        return saved.get();
+    }
+
+    public BooleanProperty savedProperty() {
+        return saved;
+    }
+
+    public void setSaved(boolean saved) {
+        this.saved.set(saved);
+    }
+
+    public LocalDate getDate() {
+        return date.get();
+    }
+
+    public ObjectProperty<LocalDate> dateProperty() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date.set(date);
+    }
 }
+
